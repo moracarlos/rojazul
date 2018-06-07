f6e204902de17f4d2fa163cd7e1516d62672bf7b
Frankeliz Nieves <frankenieves@gmail.com>
2018-06-07 17:30:22 -0400

    hotfix responsive

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+// 	$(".image-1").ready(function(){
+// 		$(".text-1").width($(".image-1").width());
+// 		$(".text-1").height($(".image-1").height());
+// 		$(".text-2").width($(".image-1").width());
+// 		$(".text-2").height($(".image-1").height());
+// 		$(".text-3").width($(".image-1").width());
+// 		$(".text-3").height($(".image-1").height());	
+
+// 	});
+
+// 	$( ".image-1" ).resize(function() {
+// 		$(".text-1").width($(".image-1").width());
+// 		$(".text-1").height($(".image-1").height());
+// 		$(".text-2").width($(".image-1").width());
+// 		$(".text-2").height($(".image-1").height());
+// 		$(".text-3").width($(".image-1").width());
+// 		$(".text-3").height($(".image-1").height());
+// 	});	
+// });
